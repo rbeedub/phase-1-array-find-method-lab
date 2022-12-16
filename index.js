07c69c5d64cc34ef4a8f88929c851c970d4bd89e
Rebecca Wollard <rbwollard@gmail.com>
2022-12-15 21:57:43 -0700

    commit tree 32355812ec7535814ba59c78c2f4c9efd198c73a

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+// function superbowlWin(gamesObject) {
+//     for (const stat of gamesObject) {
+//         if (stat.result === "W") {
+//             return stat;
+//         } 
+//     }
+// };
+
+
+// function superbowlWin(game) {
+//     return game.result === "W"
+// };
+
+// console.log(record.find(superbowlWin));
+
+function superbowlWin(gamesArray) {
+    const win = gamesArray.find(stat => stat.result === "W")
+    if (win) {
+        return win.year 
+    }else {
+        return undefined
+    }
+}
